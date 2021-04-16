b24874d5cd74cec3e5645ad7c7b28fac56e1e313
Austin Shapiro <truescarsz@gmail.com>
2021-04-16 03:36:24 -0700

    add support for querying subscriber status

--- src/main/java/me/minecraftauth/lib/AuthService.java
+++ src/main/java/me/minecraftauth/lib/AuthService.java
@@ -75,4 +75,52 @@ public class AuthService {
         }
     }
 
+    private static boolean isSubscribed(String serverToken, AccountType accountType, Object identifier, Object data) throws LookupException {
+        HttpRequest request = HttpRequest.get("https://minecraftauth.me/api/subscribed/?" + accountType.name().toLowerCase() + "=" + identifier + (data != null ? "&role=" + data : ""))
+                .userAgent("MinecraftAuthLib")
+                .authorization("Basic " + serverToken);
+
+        if (request.code() / 100 == 2) {
+            try {
+                Dynamic response = Dynamic.from(JSON_PARSER.parse(request.body()));
+                return response.dget("subscribed").convert().intoString().equals("true");
+            } catch (HttpRequest.HttpRequestException | ParseException e) {
+                throw new LookupException("Failed to parse API response", e);
+            }
+        } else {
+            throw new LookupException("MinecraftAuth server returned bad code: " + request.code());
+        }
+    }
+    /**
+     * Query if the given Discord user ID has the given Discord role
+     * @param serverToken the server authentication token to query data for
+     * @param userId the Discord user ID to query
+     * @param roleId the Discord role ID to query
+     * @return if the given Discord user has the given role
+     * @throws LookupException if the API returns abnormal error code
+     */
+    public static boolean isSubscribedDiscord(String serverToken, String userId, String roleId) throws LookupException {
+        return isSubscribed(serverToken, AccountType.DISCORD, userId, roleId);
+    }
+    /**
+     * Query if the given Patreon uid is subbed to the server token's Patreon campaign
+     * @param serverToken the server authentication token to query data for
+     * @param uid the Patreon uid to query
+     * @return if the given Patreon uid is subbed to the server token's Patreon campaign
+     * @throws LookupException if the API returns abnormal error code
+     */
+    public static boolean isSubscribedPatreon(String serverToken, int uid) throws LookupException {
+        return isSubscribed(serverToken, AccountType.PATREON, uid, null);
+    }
+    /**
+     * Query if the given Twitch uid is subbed to the server token's Twitch channel
+     * @param serverToken the server authentication token to query data for
+     * @param uid the Twitch uid to query
+     * @return if the given Twitch uid is subbed to the server token's Twitch channel
+     * @throws LookupException if the API returns abnormal error code
+     */
+    public static boolean isSubscribedTwitch(String serverToken, int uid) throws LookupException {
+        return isSubscribed(serverToken, AccountType.TWITCH, uid, null);
+    }
+
 }
